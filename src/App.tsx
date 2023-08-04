3ab8175ff0e2d868bd22413289bf2c0d2e919549
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-08-04 16:08:27 -0500

    style: Update App component styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,67 @@
-import './App.css'
-
-function App() {
-  return <></>
+const App = () => {
+  return (
+    <section className='flex h-screen flex-col items-center justify-center text-3xl text-[#284b7c]'>
+      Progreso
+      <ul className='mt-4 text-base text-[#0e0e11]'>
+        <li className='flex w-screen items-center justify-center border border-b-0 bg-red-50 last:border'>
+          <div className='flex h-20 w-full max-w-[765px] items-center justify-between bg-blue-50'>
+            <div className='flex gap-5'>
+              <div className='flex h-14 w-14 items-center justify-center rounded-full border font-semibold'>
+                A2.2
+              </div>
+              <div>
+                <p className='font-semibold'>Principiante X</p>
+                <p className='text-sm text-[#535665]'>24 clases</p>
+              </div>
+            </div>
+            <i>Icon</i>
+          </div>
+        </li>
+        <li className='flex w-screen items-center justify-center border border-b-0 bg-red-50 last:border'>
+          <div className='flex h-20 w-full max-w-[765px] items-center justify-between bg-blue-50'>
+            <div className='flex gap-5'>
+              <div className='flex h-14 w-14 items-center justify-center rounded-full border text-base font-semibold'>
+                A2.2
+              </div>
+              <div>
+                <p className='font-semibold'>Principiante X</p>
+                <p className='text-sm text-[#535665]'>24 clases</p>
+              </div>
+            </div>
+            <i>Icon</i>
+          </div>
+        </li>
+        <li className='flex w-screen items-center justify-center border border-b-0 bg-red-50 last:border'>
+          <div className='flex h-20 w-full max-w-[765px] items-center justify-between bg-blue-50'>
+            <div className='flex gap-5'>
+              <div className='flex h-14 w-14 items-center justify-center rounded-full border text-base font-semibold'>
+                A2.2
+              </div>
+              <div>
+                <p className='font-semibold'>Principiante X</p>
+                <p className='text-sm text-[#535665]'>24 clases</p>
+              </div>
+            </div>
+            <i>Icon</i>
+          </div>
+        </li>
+        <li className='flex w-screen items-center justify-center border border-b-0 bg-red-50 last:border'>
+          <div className='flex h-20 w-full max-w-[765px] items-center justify-between bg-blue-50'>
+            <div className='flex gap-5'>
+              <div className='flex h-14 w-14 items-center justify-center rounded-full border text-base font-semibold'>
+                A2.2
+              </div>
+              <div>
+                <p className='font-semibold'>Principiante X</p>
+                <p className='text-sm text-[#535665]'>24 clases</p>
+              </div>
+            </div>
+            <i>Icon</i>
+          </div>
+        </li>
+      </ul>
+    </section>
+  )
 }
 
 export default App
