641edc9b5a37f9d832675b76d03117d58fc665ee
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-08-04 16:18:10 -0500

    feat: Add QueryProvider component

---
+++ src/components/QueryProvider.tsx
@@ -0,0 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+})
+
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+export default QueryProvider
