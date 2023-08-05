bce6e65c2d6010659b43b84bcbbb911a50622951
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-08-05 09:39:06 -0500

    refactor: optimize code and improve readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,52 @@
+import { useState } from 'react'
+import { ImSpinner9 } from 'react-icons/im'
+import useProgress from './hooks/useProgress'
+
+import 'react-circular-progressbar/dist/styles.css'
+import { Module } from './components/Module'
+
 const App = () => {
+  const [selectedID, setSelectedID] = useState('')
+  const { data, isLoading, isError } = useProgress()
+
+  if (isLoading) {
+    return (
+      <section className='flex h-screen items-center justify-center text-3xl text-[#284b7c]'>
+        <ImSpinner9 className='animate-spin' />
+      </section>
+    )
+  }
+
+  if (isError) {
+    return (
+      <section className='flex h-screen items-center justify-center text-3xl text-[#284b7c]'>
+        <p>Something went wrong</p>
+      </section>
+    )
+  }
+
   return (
-    <section className='flex h-screen flex-col items-center justify-center text-3xl text-[#284b7c]'>
+    <section className='mt-20 flex flex-col items-center justify-center overflow-x-hidden text-3xl text-[#284b7c]'>
       Progreso
-      <ul className='mt-4 text-base text-[#0e0e11]'>
-        <li className='flex w-screen items-center justify-center border border-b-0 bg-red-50 last:border'>
-          <div className='flex h-20 w-full max-w-[765px] items-center justify-between bg-blue-50'>
-            <div className='flex gap-5'>
-              <div className='flex h-14 w-14 items-center justify-center rounded-full border font-semibold'>
-                A2.2
-              </div>
-              <div>
-                <p className='font-semibold'>Principiante X</p>
-                <p className='text-sm text-[#535665]'>24 clases</p>
-              </div>
-            </div>
-            <i>Icon</i>
-          </div>
-        </li>
-        <li className='flex w-screen items-center justify-center border border-b-0 bg-red-50 last:border'>
-          <div className='flex h-20 w-full max-w-[765px] items-center justify-between bg-blue-50'>
-            <div className='flex gap-5'>
-              <div className='flex h-14 w-14 items-center justify-center rounded-full border text-base font-semibold'>
-                A2.2
-              </div>
-              <div>
-                <p className='font-semibold'>Principiante X</p>
-                <p className='text-sm text-[#535665]'>24 clases</p>
-              </div>
-            </div>
-            <i>Icon</i>
-          </div>
-        </li>
-        <li className='flex w-screen items-center justify-center border border-b-0 bg-red-50 last:border'>
-          <div className='flex h-20 w-full max-w-[765px] items-center justify-between bg-blue-50'>
-            <div className='flex gap-5'>
-              <div className='flex h-14 w-14 items-center justify-center rounded-full border text-base font-semibold'>
-                A2.2
-              </div>
-              <div>
-                <p className='font-semibold'>Principiante X</p>
-                <p className='text-sm text-[#535665]'>24 clases</p>
-              </div>
-            </div>
-            <i>Icon</i>
-          </div>
-        </li>
-        <li className='flex w-screen items-center justify-center border border-b-0 bg-red-50 last:border'>
-          <div className='flex h-20 w-full max-w-[765px] items-center justify-between bg-blue-50'>
-            <div className='flex gap-5'>
-              <div className='flex h-14 w-14 items-center justify-center rounded-full border text-base font-semibold'>
-                A2.2
-              </div>
-              <div>
-                <p className='font-semibold'>Principiante X</p>
-                <p className='text-sm text-[#535665]'>24 clases</p>
-              </div>
-            </div>
-            <i>Icon</i>
-          </div>
-        </li>
+      <ul className='mt-4 cursor-pointer select-none text-base text-[#0e0e11]'>
+        {data?.map((module) => (
+          <li
+            key={module.id}
+            id={module.id}
+            className='flex w-screen flex-col items-center justify-center border border-b-0 py-1 last:border'
+            onClick={() => {
+              if (selectedID === module.id) setSelectedID('')
+              else setSelectedID(module.id)
+            }}
+          >
+            <Module
+              key={module.id}
+              module={module}
+              data={data}
+              selectedID={selectedID}
+            />
+          </li>
+        ))}
       </ul>
     </section>
   )
