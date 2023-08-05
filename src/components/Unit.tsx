01c1b78f2657885d63f70207ccb6746d104fb83b
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-08-05 09:39:40 -0500

    fix: Add mutation prop to Session component

--- src/components/Unit.tsx
+++ src/components/Unit.tsx
@@ -1,13 +1,16 @@
+import { UseMutationResult } from '@tanstack/react-query'
 import { Progress, ProgressItem } from '../interfaces/progress'
+import { UpdateProgress } from '../utils/updateProgress'
 import { Session } from './Session'
 
 interface Props {
   data: Progress[]
   module: Progress
   unit: ProgressItem
+  mutation: UseMutationResult<unknown, unknown, UpdateProgress, unknown>
 }
 
-export const Unit = ({ unit, data, module }: Props) => {
+export const Unit = ({ unit, data, module, mutation }: Props) => {
   return (
     <div
       className='flex w-full max-w-[765px] flex-col justify-center'
@@ -17,6 +20,7 @@ export const Unit = ({ unit, data, module }: Props) => {
       <ul>
         {unit.items.map((session) => (
           <Session
+            mutation={mutation}
             key={session.id}
             session={session}
             data={data}
